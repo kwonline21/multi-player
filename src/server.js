4c1ce2fdb5475e392929c56aed397dbb574c5b79
Kwon <kwonline21@gmail.com>
2024-10-30 10:44:19 +0900

    socket events

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import net from 'net';
 import { PORT, HOST } from './constants/env.js';
+import { onConnection } from './events/onConnection.js';
 
-const server = net.createServer();
+const server = net.createServer(onConnection);
 
 server.listen(PORT, HOST, () => {
   console.log(`Server running and listening at ${HOST}:${PORT}`);
